6d26a764330dc998bea5743f92329f7fb9d3db3f
esodermark <sodermark@live.se>
2020-02-26 15:37:24 +0100

    custom post type changes, added content function for CPT to function.php

--- app/public/wp-content/themes/palmeria-child/functions.php
+++ app/public/wp-content/themes/palmeria-child/functions.php
@@ -13,3 +13,26 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
+
+
+add_filter ('the_content', 'prefix_add_content');
+
+// adds custom content to sales_item
+function prefix_add_content ($content){
+    global $post;
+    if ($post->post_type == 'sales_item') {
+
+        $number_of_rooms = get_post_meta( get_the_ID(), 'number_of_rooms', true );
+        $square_meters = get_post_meta( get_the_ID(), 'square_meters', true );
+        $address = get_post_meta( get_the_ID(), 'address', true );
+        $initial_bid = get_post_meta( get_the_ID(), 'initial_bid', true );
+
+        $before = '<h3 class="address">' . $address . '</h3>';
+        $before .= '<ul class="estateInfo"><li><dl><dt>' . $number_of_rooms .'</dt><dd>rooms</dd></dl></li>';
+        $before .= '<li><dl><dt>' . $square_meters . '<span class="sqm">sqm</span></dt><dd>interior</dd></dl></li>';
+        $before .= '<li><dl><dt>' . $initial_bid . '</dt><dd>kronor</dd></dl></li></ul>';
+        $after = "";
+        $content = $before . $content . $after;
+    }
+    return $content;
+}
