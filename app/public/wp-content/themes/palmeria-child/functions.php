7780fc2913f14bf60366590a810d9a297a4f4cba
esodermark <sodermark@live.se>
2020-02-27 19:15:33 +0100

    added tags, changed how initial bid is displayed

--- app/public/wp-content/themes/palmeria-child/functions.php
+++ app/public/wp-content/themes/palmeria-child/functions.php
@@ -27,12 +27,22 @@ function prefix_add_content ($content){
         $address = get_post_meta( get_the_ID(), 'address', true );
         $initial_bid = get_post_meta( get_the_ID(), 'initial_bid', true );
 
+       $bid = substr($initial_bid, 2, -3);
+        $bid = number_format($bid);
+        $pound =  substr($initial_bid, 0, 2);
+        $new = $pound . $bid;
+
+        
+
         $before = '<h3 class="address">' . $address . '</h3>';
         $before .= '<ul class="estateInfo"><li><dl><dt>' . $number_of_rooms .'</dt><dd>rooms</dd></dl></li>';
         $before .= '<li><dl><dt>' . $square_meters . '<span class="sqm">sqm</span></dt><dd>interior</dd></dl></li>';
-        $before .= '<li><dl><dt>' . $initial_bid . '</dt><dd>initial bid</dd></dl></li></ul>';
+        $before .= '<li><dl><dt>' . $new . '</dt><dd>initial bid</dd></dl></li></ul>';
+
+        
         $after = "";
         $content = $before . $content . $after;
     }
     return $content;
 }
+
