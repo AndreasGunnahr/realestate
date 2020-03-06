940a01049b31dafac3ddd8c2867dbf8534b26932
Andreas Gunnahr <Andreas.Gunnahr@hotmail.com>
2020-03-06 14:49:37 +0100

    first page

--- app/public/wp-content/themes/palmeria-child/functions.php
+++ app/public/wp-content/themes/palmeria-child/functions.php
@@ -1,38 +1,38 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 
-function my_theme_enqueue_styles() {
- 
+function my_theme_enqueue_styles()
+{
     $parent_style = 'parent-style';
- 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+
+    wp_enqueue_style($parent_style, get_template_directory_uri().'/style.css');
+    wp_enqueue_style('child-style',
+        get_stylesheet_directory_uri().'/style.css',
+        array($parent_style),
         wp_get_theme()->get('Version')
     );
 }
 
-
-add_filter ('the_content', 'prefix_add_content');
+add_filter('the_content', 'prefix_add_content');
 
 // adds custom content to sales_item
-function prefix_add_content ($content){
+function prefix_add_content($content)
+{
     global $post;
     if ($post->post_type == 'sales_item') {
+        $number_of_rooms = get_post_meta(get_the_ID(), 'number_of_rooms', true);
+        $square_meters = get_post_meta(get_the_ID(), 'square_meters', true);
+        $address = get_post_meta(get_the_ID(), 'address', true);
+        $initial_bid = get_post_meta(get_the_ID(), 'initial_bid', true);
 
-        $number_of_rooms = get_post_meta( get_the_ID(), 'number_of_rooms', true );
-        $square_meters = get_post_meta( get_the_ID(), 'square_meters', true );
-        $address = get_post_meta( get_the_ID(), 'address', true );
-        $initial_bid = get_post_meta( get_the_ID(), 'initial_bid', true );
-
-        $before = '<h3 class="address">' . $address . '</h3>';
-        $before .= '<ul class="estateInfo"><li><dl><dt>' . $number_of_rooms .'</dt><dd>rooms</dd></dl></li>';
-        $before .= '<li><dl><dt>' . $square_meters . '<span class="sqm">sqm</span></dt><dd>interior</dd></dl></li>';
-        $before .= '<li><dl><dt>' . $initial_bid . '</dt><dd>initial bid</dd></dl></li></ul>';
-        $after = "";
-        $content = $before . $content . $after;
+        $before = '<h3 class="address">'.$address.'</h3>';
+        $before .= '<ul class="estateInfo"><li><dl><dt>'.$number_of_rooms.'</dt><dd>rooms</dd></dl></li>';
+        $before .= '<li><dl><dt>'.$square_meters.'<span class="sqm">sqm</span></dt><dd>interior</dd></dl></li>';
+        $before .= '<li><dl><dt>'.$initial_bid.'</dt><dd>initial bid</dd></dl></li></ul>';
+        $after = '';
+        $content = $before.$content.$after;
     }
+
     return $content;
 }
