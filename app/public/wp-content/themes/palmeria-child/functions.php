c69159240ec4aaa70134fe484531abccf2069c50
esodermark <sodermark@live.se>
2020-03-08 11:09:10 +0100

    updated bid format in post_meta, added styling to sidebar, displayed property_view_date on sales item posts

--- app/public/wp-content/themes/palmeria-child/functions.php
+++ app/public/wp-content/themes/palmeria-child/functions.php
@@ -37,21 +37,17 @@ function prefix_add_content($content)
         $square_meters = get_post_meta(get_the_ID(), 'square_meters', true);
         $address = get_post_meta(get_the_ID(), 'address', true);
         $initial_bid = get_post_meta(get_the_ID(), 'initial_bid', true);
+        $view_date = get_post_meta(get_the_ID(), 'property_view_date', true);
 
-        $number_of_rooms = get_post_meta(get_the_ID(), 'number_of_rooms', true);
-        $square_meters = get_post_meta(get_the_ID(), 'square_meters', true);
-        $address = get_post_meta(get_the_ID(), 'address', true);
-        $initial_bid = get_post_meta(get_the_ID(), 'initial_bid', true);
-
-        $bid = substr($initial_bid, 2, -3);
-        $bid = number_format($bid);
-        $pound = substr($initial_bid, 0, 2);
-        $new = $pound.$bid;
+        $str = $view_date;
+        $str = $str[0].$str[1].$str[2].$str[3].'/'.$str[4].$str[5].'/'.$str[6].$str[7];
+        $propert_view_date = date('F jS', strtotime($str));
 
         $before = '<h3 class="address">'.$address.'</h3>';
         $before .= '<ul class="estateInfo"><li><dl><dt>'.$number_of_rooms.'</dt><dd>rooms</dd></dl></li>';
         $before .= '<li><dl><dt>'.$square_meters.'<span class="sqm">sqm</span></dt><dd>interior</dd></dl></li>';
-        $before .= '<li><dl><dt>'.$new.'</dt><dd>initial bid</dd></dl></li></ul>';
+        $before .= '<li><dl><dt>'.$initial_bid.'</dt><dd>initial bid</dd></dl></li>';
+        $before .= '<li><dl><dt class="date">'.$propert_view_date.'</dt><dd>viewing</dd></dl></li></ul>';
 
         $after = '';
         $content = $before.$content.$after;
